3ab0ea30f3ce6346af1ff317e1015a75cfc6e182
wheelstep <stefano.pernat@gmail.com>
2021-03-15 17:48:55 +0100

    Aggiungere i file di progetto.

---
+++ TombstoneRedefined/TombstoneRedefined.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using ValheimPlus.Configurations;
+
+namespace TombstoneRedefined
+{
+    [BepInPlugin("net.bdew.valheim.tombstoneredefined", "TombstoneRedefined", "0.0.1")]
+    public class TombstoneRedefined: BaseUnityPlugin
+    {
+        private static ManualLogSource LogSource;
+
+        void Awake()
+        {
+            LogSource = Logger;
+            LogSource.LogInfo(String.Format("TombstoneRedefined is Awake"));
+            Harmony.CreateAndPatchAll(typeof(TombstoneRedefined));
+            //Player 
+        }
+    }
+}
